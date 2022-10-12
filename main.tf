3a108affd8e16e9feaa4c79da81212e1a86d9089
kolosovpetro <kolosovp94@gmail.com>
2022-10-12 21:53:53 +0200

    account name fixed

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "rg-hello-azure-tf"
-    storage_account_name = "pkolosovtfstate"
+    storage_account_name = "pkolosovtfstate01"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
