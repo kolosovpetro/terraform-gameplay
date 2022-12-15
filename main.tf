ec90e3ad417e799347fd7016c3b2a71e7b791c80
kolosovpetro <kolosovp94@gmail.com>
2022-12-15 12:16:47 +0100

    main tf updated

--- main.tf
+++ main.tf
@@ -148,7 +148,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 }
 
 resource "azurerm_windows_web_app" "webapp" {
-  name                = "app-messenger-d01"
+  name                = "app-messenger-d03"
   location            = azurerm_resource_group.rg_messenger.location
   resource_group_name = azurerm_resource_group.rg_messenger.name
   service_plan_id     = azurerm_service_plan.appserviceplan.id
@@ -164,30 +164,3 @@ resource "azurerm_windows_web_app" "webapp" {
 }
 
 # create app service process ends
-
-resource "azurerm_sql_server" "example" {
-  name                         = "myexamplesqlserver"
-  resource_group_name          = azurerm_resource_group.rg_messenger.name
-  location                     = azurerm_resource_group.rg_messenger.location
-  version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
-
-  tags = {
-    environment = "production"
-  }
-}
-
-resource "azurerm_sql_database" "example" {
-  name                             = "myexamplesqldatabase"
-  resource_group_name              = azurerm_resource_group.rg_messenger.name
-  location                         = azurerm_resource_group.rg_messenger.location
-  server_name                      = azurerm_sql_server.example.name
-  requested_service_objective_name = "Free"
-
-
-
-  tags = {
-    environment = "production"
-  }
-}
